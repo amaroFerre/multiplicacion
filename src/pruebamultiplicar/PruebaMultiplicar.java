b97e48403ebf080ff8cbed0caa72136fc9300ec4
Amarico <Amarico@EquipoAcer>
2018-08-20 10:01:27 +0200

    Creo un nuevo objeto de la clase multiplicar

---
+++ src/pruebamultiplicar/PruebaMultiplicar.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebamultiplicar;
+
+/**
+ *
+ * @author Amarico
+ */
+public class PruebaMultiplicar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //creo un nuevo objeta de la clase multiplicar
+        
+        
+    }
+    
+}
